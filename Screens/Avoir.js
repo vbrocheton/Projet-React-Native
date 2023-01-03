97456afe474181953efacb90fed629a7c7a79ec3
Vincent Brocheton <vinc.brocheton@gmail.com>
2023-01-03 17:06:22 +0100

    Mise en place de la navigation

---
+++ Screens/Avoir.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AVoir = ({
+    params,
+}) => (
+    <View>
+        <Text>AVoir</Text>
+    </View>
+);
+
+export default AVoir;
---
+++ Screens/Compte.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Compte = ({
+    params,
+}) => (
+    <View>
+        <Text>Compte</Text>
+    </View>
+);
+
+export default Compte;
