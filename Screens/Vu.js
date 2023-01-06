fca5cc9ccf13bce283f10c8310b40bbab01d1966
Vincent Brocheton <vinc.brocheton@gmail.com>
2023-01-06 12:24:16 +0100

    Page A voir etc... Plus Insertion en base de données à l'inscription

--- Screens/Vu.js
+++ Screens/Vu.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View, FlatList } from 'react-native';
 import { db } from '../firebase-auth';
 import {
   collection,
@@ -10,9 +10,13 @@ import {
   setDoc,
   doc,
 } from 'firebase/firestore';
+import { authentication } from '../firebase-auth';
+import SerieItem from '../Components/SerieItem';
 
-const Vu = ({ params }) => {
+const Vu = () => {
   const [series, setSeries] = useState('');
+
+  const [loading, setLoading] = useState(true);
   const fetchUser = async () => {
     const q = query(
       collection(db, 'User'),
@@ -24,15 +28,31 @@ const Vu = ({ params }) => {
       user.id = doc.id;
       setSeries(user.vu);
     });
+    setLoading(false);
   };
   useEffect(() => {
     fetchUser().then((r) => '');
   }, []);
   return (
-    <View>
-      <Text>Vu</Text>
-    </View>
+    <>
+      {loading && <View style={styles.loadingContent}><ActivityIndicator size={"large"}/></View>}
+      {!loading && <FlatList
+        numColumns={3}
+          data={series}
+          keyExtractor={(item) => item.id}
+          renderItem={(item) => <SerieItem serie={item.item} />}
+        />}
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+    loadingContent: {
+        flex:1,
+        height:'100%',
+        justifyContent:"center",
+        alignItems:"center"
+    }
+})
 
 export default Vu;
